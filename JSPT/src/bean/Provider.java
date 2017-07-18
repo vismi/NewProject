4c8f2203966b6463c4e4e4f38e8b3889029c8dc3
vismi <vismi.25@gmail.com>
2017-07-18 17:13:36 +0530

    18 July 2017

---
+++ JSPT/src/bean/Provider.java
@@ -0,0 +1,10 @@
+package bean;
+
+
+public interface Provider {  
+String DRIVER="com.mysql.jdbc.Driver";  
+String CONNECTION_URL="jdbc:mysql://localhost:3306/Vismita";  
+String USERNAME="root";  
+String PASSWORD="niit@123";  
+ 
+}  
---
+++ SpringProjV/src/tutorialspoint/HelloWorld.java
@@ -0,0 +1,26 @@
+package tutorialspoint;
+
+
+public class HelloWorld {
+   private String message;
+ private int x, y;
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println(x+y);
+   }
+public int getX() {
+	return x;
+}
+public void setX(int x) {
+	this.x = x;
+}
+public int getY() {
+	return y;
+}
+public void setY(int y) {
+	this.y = y;
+}
+}
+
